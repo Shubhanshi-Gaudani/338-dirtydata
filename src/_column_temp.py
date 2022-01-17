77ec7fd942fe776e712a00713925bada906a5480
mveillon <49701743+mveillon@users.noreply.github.com>
2022-01-16 19:56:05 -0600

    added column docs

--- src/_column_temp.py
+++ src/_column_temp.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 
 class Column:
+    """Temporary container class for column data.
+    
+    Put any information your predicates need about the columns in
+    this class and we can write some code to analyze the data to find
+    that information later
+    """
     def __init__(self, mean, stddev):
         self.mean = mean
         self.stddev = stddev
