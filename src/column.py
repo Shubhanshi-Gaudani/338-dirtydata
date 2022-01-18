6b69f4638fcbeced501c8d417e5efa6eef298b7e
mveillon <49701743+mveillon@users.noreply.github.com>
2022-01-18 12:10:27 -0600

    docstrings!

--- src/column.py
+++ src/column.py
@@ -11,20 +11,63 @@ class Column:
         self.column_type = self.get_col_type(col)
 
     def get_mean(self, col):
+        """Returns the mean of the column.
+        
+        Args:
+            col (np.array) : an array of strings, some of which might be
+                numeric, some of which might not
+                
+        Returns:
+            mean (float) : the mean of the numeric cells in col
+        """
         raise NotImplementedError
     
     def get_stddev(self, col):
+        """Returns the standard deviation of the column.
+        
+        Args:
+            col (np.array) : an array of strings, some of which might be
+                numeric, some of which might not
+                
+        Returns:
+            stddev (float) : the standard deviation of the numeric cells in col
+        """
         std = np.std(col)
-        return std;
+        return std
 
     def get_median(self, col):
+        """Returns the median of the column.
+        
+        Args:
+            col (np.array) : an array of strings, some of which might be
+                numeric, some of which might not
+                
+        Returns:
+            median (float) : the median of the numeric cells in col
+        """
         return np.median(col)
 
     def get_mode(self, col):
+        """Returns the mode of the column.
+        
+        Args:
+            col (np.array) : an array of strings, some of which might be
+                numeric, some of which might not
+                
+        Returns:
+            mode (float) : the mode of the numeric cells in col
+        """
         raise NotImplementedError
 
     def get_col_type(self, col):
-        """returns the most common column type - either 'num' or 'alpha'."""
+        """Returns the most common column type - either 'num' or 'alpha'.
+        
+        Args:
+            col (np.array) : an array of strings
+            
+        Returns:
+            type (string) : either 'num' or 'alpha'
+        """
         al_num_counts = [0, 0]
         for row in range(col.shape[0]):
             al_num_counts[int(can_be_float(col[row]))] += 1
