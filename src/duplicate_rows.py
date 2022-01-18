9232a2003d91896ae50739d2ee43fccbe6ea0741
PrabhavJain10 <67673958+PrabhavJain10@users.noreply.github.com>
2022-01-18 11:37:50 -0600

    Added duplicate rows function

---
+++ src/duplicate_rows.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from .utilities import can_be_float
+
+def duplicate_row(data):
+    """Takes a whole dataset, returns which rows if any are duplicates.
+
+    Args:
+        data (pd) : a panda datafrae
+
+    Returns:
+        dup_rows (pd) : dataframe of duplicate rows
+    """
+
+    dup_rows = data[data.duplicated()]
+
+    return dup_rows
