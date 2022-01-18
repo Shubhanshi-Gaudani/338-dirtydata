42335a78c52d3fa56dd2a27d3316a268fd177d4a
mveillon <49701743+mveillon@users.noreply.github.com>
2022-01-18 12:04:31 -0600

    small changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from .num_outliers import is_outlier
-from ._column_temp import Column
+from .column import Column
 from .utilities import can_be_float, is_float_char
--- src/all_dirty_cells.py
+++ src/all_dirty_cells.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from .num_outliers import is_outlier
-from ._column_temp import Column
+from .Column import Column
 
 def all_dirty_cells(csv_mat):
     preds = [is_outlier]
