f8b1c727d90e19573a9031d5ec52a886f510dea2
Vishwani Sati <91351468+vishwanisati@users.noreply.github.com>
2022-01-25 00:32:35 -0600

    Update validdate.py

--- src/validdate.py
+++ src/validdate.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def isDateValid(date, pattern = "%d/%m/%y"):
+def isDateValid(date, pattern = "%d/%m/%Y"):
     try:
         datetime.strptime(date, pattern)
         return False
