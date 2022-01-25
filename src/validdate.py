ceda14cd959d85410efdfe0c4f9c91916b395cf6
Vishwani Sati <91351468+vishwanisati@users.noreply.github.com>
2022-01-25 00:25:49 -0600

    Update validdate.py

--- src/validdate.py
+++ src/validdate.py
@@ -3,20 +3,6 @@ from datetime import datetime
 def isDateValid(date, pattern = "%d/%m/%y"):
     try:
         datetime.strptime(date, pattern)
-        return True
-    except ValueError:
         return False
-with open("filename.csv") as f:
-    # split file into lines
-    lines = f.readlines()
-    lines = [x.replace("\n", "") for x in lines]
-    header = lines[0]
-    rows = lines[1:]
-    for rowNumber, row in enumerate(rows, 1):
-        columns = line.split(",")
-        gotValidDate = False
-        for column in columns:
-            if isDateValid(column):
-                gotValidDate = True
-        if gotValidDate:
-            print(f"Row {rowNumber} got at least one valid date")
+    except ValueError:
+        return True
