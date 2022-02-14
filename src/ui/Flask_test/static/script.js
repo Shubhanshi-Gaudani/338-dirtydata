a5a3edb094ae90fdfac248df7971bcf7d2f472cf
Shubhanshi-Gaudani <72951422+Shubhanshi-Gaudani@users.noreply.github.com>
2022-02-13 20:52:20 -0600

    config page redirect
    config page works independently but home page submit button doesn't redirect to it currently

--- src/ui/Flask_test/static/script.js
+++ src/ui/Flask_test/static/script.js
@@ -33,5 +33,16 @@ function Upload() {
     }
 }
 
+function RetrieveConfigCheckboxes() {
+    const form = document.querySelector('form');
 
+    form.addEventListener('submit', e => {
+        e.preventDefault();
 
+    const values = Array.from(document.querySelectorAll('input[type=checkbox]:checked'))
+        .map(item => item.value)
+        .join(',');
+
+    console.log(`test.com/addtocart?${values}`);
+});
+}
