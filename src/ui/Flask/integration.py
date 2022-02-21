d397c9c0da858d88eccdf8dea8be06498e8072a6
mveillon <49701743+mveillon@users.noreply.github.com>
2022-02-20 20:52:51 -0600

    doesnt work yet

--- src/ui/Flask/integration.py
+++ src/ui/Flask/integration.py
@@ -6,6 +6,8 @@ from src import _ALL_PREDS
 
 CLEAN_NAME = 'cleaned.csv'
 CLEAN_PATH = data_path() + '/' + CLEAN_NAME
+DUP_ROW_IND = 0
+DUP_COL_IND = 1
 
 def get_dirty(mat):
     """Reads mat and finds the dirty cells.
@@ -20,11 +22,15 @@ def get_dirty(mat):
             dirty failed. reasons[i] is the reason why dirty[i] failed
         cols (list) : a list of Column objects
     """
+    preds, dups = get_preds()
+    mat = delete_dupes(mat, 
+                       del_rows = dups[DUP_ROW_IND],
+                       del_cols = dups[DUP_COL_IND])
     return all_dirty_cells(mat,
                            parallel = True,
                            return_cols = True,
                            header = has_header(mat),
-                           preds = get_preds())
+                           preds = preds)
 
 def delete_dupes(mat, del_rows = True, del_cols = True):
     """Deletes duplicate rows and columns.
@@ -35,7 +41,7 @@ def delete_dupes(mat, del_rows = True, del_cols = True):
         del_cols (bool) : whether to delete duplicate columns. Default is True.
 
     Returns:
-        None
+        new_mat (np.array) : the updated matrix
     """
     # duplicate rows 
     if(del_rows):
@@ -45,6 +51,8 @@ def delete_dupes(mat, del_rows = True, del_cols = True):
     if(del_cols):
         dupes = duplicate_columns(mat)
         mat = np.delete(mat, dupes, 1)
+    
+    return mat
 
 def save_clean(mat, inds, reasons, cols):
     """Cleans mat and saves it to CLEAN_PATH.
@@ -82,6 +90,8 @@ def pred_names_to_objs(names):
 
     Returns:
         preds (list) : a list of rules to use when finding dirty cells
+        dup_lst (list) : a list of '0' or '1', corresponding to whether the user
+            wants to delete duplicate rows and columns
     """
     mapping = {IsNA : 'checkNA',
                IsIncorrectDataType : 'IsIncorrectDataType',
@@ -95,8 +105,10 @@ def pred_names_to_objs(names):
     for pred in _ALL_PREDS:
         if mapping[pred] in name_set:
             res.append(pred)
-    
-    return res
+
+    dupes = ['DuplicateRows', 'DuplicateColumns']
+    dup_lst = [ el in name_set for el in dupes ]
+    return res, dup_lst
 
 def get_preds():
     """Returns the user's selected predicates."""
