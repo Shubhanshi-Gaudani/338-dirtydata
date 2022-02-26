f3be78845b1d15a5c06340abf1f12cd2ff91b0eb
PrabhavJain10 <67673958+PrabhavJain10@users.noreply.github.com>
2022-02-25 18:14:07 -0600

    wrong category fixed

--- src/rules/wrong_category.py
+++ src/rules/wrong_category.py
@@ -1,5 +1,9 @@
 import numpy as np
+import spacy
 from .rule_base import RuleBaseClass
+  
+import en_core_web_sm
+nlp = en_core_web_sm.load()
 
 _COUNT_PER_100_LINES = 2
 _NUM_CATS_PER_100 = 2
@@ -19,3 +23,23 @@ class WrongCategory (RuleBaseClass):
     def message(self, cell_str, col):
         return ('This row appears to have a small number of categories, but ' +
                 f'{cell_str} is not one of them.')
+
+    def clean(self, inds, sheet, col, all_dirty):
+        dict = col.by_count
+        word = sheet[tuple(inds)]
+        cur_tok = nlp(str(word))
+        max_sim = 0
+        best_guess = word
+        words = ""
+        for i, (j,k) in enumerate(dict.items()):
+            if k >=  _COUNT_PER_100_LINES + 1:
+                words = words + " " + j
+      
+        tokens = nlp(words)
+
+        for token in tokens:
+            if cur_tok.similarity(token) > max_sim:
+                max_sim = cur_tok.similarity(token)
+                best_guess = token.text
+        return best_guess
+        # https://www.geeksforgeeks.org/python-word-similarity-using-spacy/
