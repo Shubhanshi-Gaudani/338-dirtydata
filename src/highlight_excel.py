d64a7989d97c1303710cba5a8b0f0594963d846a
Shubhanshi-Gaudani <72951422+Shubhanshi-Gaudani@users.noreply.github.com>
2022-03-01 11:22:37 -0600

    life is no more hell, i can now highlight on excel

--- src/highlight_excel.py
+++ src/highlight_excel.py
@@ -1,4 +1,25 @@
 import pandas as pd
+import xlwings as xw
+
 
 def highlight_excel():
+
+    #Define the colours that we want in the highlighted cells:
     
+    Excellent = (0, 245, 57)
+    Better = (93, 245, 128)
+    Good = (238, 242, 0)
+    Average = (255, 92, 92)
+    Bad = (255, 0, 0)
+
+    #
+    file_name = 'test_sheets/temp.xlsx'
+    wb = xw.Book(file_name)
+    xl_sheet = wb.sheets['Sheet1']
+    xl_sheet.range("A1:A10").color = Excellent
+    xl_sheet.range("B1:B10").color = Better
+    xl_sheet.range("C1:C10").color = Good
+    xl_sheet.range("D1:D10").color = Average
+    xl_sheet.range("E1:E10").color = Bad
+
+highlight_excel()
